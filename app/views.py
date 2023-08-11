4c21fa8c75baeabb13e3c6a4a87f6fed18a29ad3
prasanth <prasanthsenthilkumaran@gmail.com>
2023-08-11 12:49:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def parent_mdb4(request):
+    return render(request,'parent_mdb4.html')
+
+def child(request):
+    return(request,'child.html')
